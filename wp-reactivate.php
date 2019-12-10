5b32b88c9281eb2ab54bda6bcf0e06102cbe985a
Ali <ali@Alis-MacBook.local>
2019-12-10 12:42:22 +0100

    Added ReactActive, Bootstrap

---
+++ wp-reactivate.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Startup-Without-Borders-Plugin
+ *
+ *
+ * @package   Startup-Without-Borders-Plugin
+ * @author    Ali Shadman,Marco Cuomo,Yousef Saghir
+ * @license   GPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Startup-Without-Borders-Plugin
+ * Description:       React WordPress plugins
+ * Author:            Ali Shadman,Marco Cuomo,Yousef Saghir
+ * Text Domain:       Startup-Without-Borders-Plugin
+ * License:           GPL-3.0
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ */
+
+
+namespace Pangolin\WPR;
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'WP_REACTIVATE_VERSION', '1.0.2' );
+
+
+/**
+ * Autoloader
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ *
+ *  * @since 1.0.0
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = __NAMESPACE__;
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/includes/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+/**
+ * Initialize Plugin
+ *
+ * @since 1.0.0
+ */
+function init() {
+	$wpr = Plugin::get_instance();
+	$wpr_shortcode = Shortcode::get_instance();
+	$wpr_admin = Admin::get_instance();
+	$wpr_rest = Endpoint\Example::get_instance();
+}
+add_action( 'plugins_loaded', 'Pangolin\\WPR\\init' );
+
+
+
+/**
+ * Register the widget
+ *
+ * @since 1.0.0
+ */
+function widget_init() {
+	return register_widget( new Widget );
+}
+add_action( 'widgets_init', 'Pangolin\\WPR\\widget_init' );
+
+function add_cors_http_header(){
+    header("Access-Control-Allow-Origin: *");
+}
+add_action('init','add_cors_http_header');
+
+/**
+ * Register activation and deactivation hooks
+ */
+register_activation_hook( __FILE__, array( 'Pangolin\\WPR\\Plugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Pangolin\\WPR\\Plugin', 'deactivate' ) );
+
