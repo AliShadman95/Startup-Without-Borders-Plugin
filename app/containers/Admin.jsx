84fb0eb7e1724296d447f9dff708b5584c2e8d0c
Ali <ali@Alis-MacBook.local>
2019-12-16 00:56:16 +0100

    removed placeholder functions

--- app/containers/Admin.jsx
+++ app/containers/Admin.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-/* import fetchWP from "../utils/fetchWP"; */
+
 import Navbar from "../containers/Navbar";
 
 export default class Admin extends Component {
@@ -9,75 +9,10 @@ export default class Admin extends Component {
     super(props);
 
     this.state = {
-      exampleSetting: "",
-      savedExampleSetting: ""
+      
     };
 
-    /* this.fetchWP = new fetchWP({
-      restURL: this.props.wpObject.api_url,
-      restNonce: this.props.wpObject.api_nonce
-    });
-
-    this.getSetting(); */
-  }
-
-  getSetting = () => {
-    this.fetchWP.get("example").then(
-      json =>
-        this.setState({
-          exampleSetting: json.value,
-          savedExampleSetting: json.value
-        }),
-      err => console.log("error", err)
-    );
-  };
-
-  updateSetting = () => {
-    this.fetchWP
-      .post("example", { exampleSetting: this.state.exampleSetting })
-      .then(
-        json => this.processOkResponse(json, "saved"),
-        err => console.log("error", err)
-      );
-  };
-
-  deleteSetting = () => {
-    this.fetchWP.delete("example").then(
-      json => this.processOkResponse(json, "deleted"),
-      err => console.log("error", err)
-    );
-  };
-
-  processOkResponse = (json, action) => {
-    if (json.success) {
-      this.setState({
-        exampleSetting: json.value,
-        savedExampleSetting: json.value
-      });
-    } else {
-      console.log(`Setting was not ${action}.`, json);
-    }
-  };
-
-  updateInput = event => {
-    this.setState({
-      exampleSetting: event.target.value
-    });
-  };
-
-  handleSave = event => {
-    event.preventDefault();
-    if (this.state.exampleSetting === this.state.savedExampleSetting) {
-      console.log("Setting unchanged");
-    } else {
-      this.updateSetting();
-    }
-  };
-
-  handleDelete = event => {
-    event.preventDefault();
-    this.deleteSetting();
-  };
+    
 
   render() {
     return (
